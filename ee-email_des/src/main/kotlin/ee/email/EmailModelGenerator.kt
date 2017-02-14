6b883322dca59e528767a32cc44edbd6ad8b1e61
Eugen Eisler <eoeisler@gmail.com>
2017-02-14 11:23:17 +0100

    Introduce DesignKotlinContextFactory

--- ee-email_des/src/main/kotlin/ee/email/EmailModelGenerator.kt
+++ ee-email_des/src/main/kotlin/ee/email/EmailModelGenerator.kt
@@ -1,9 +1,10 @@
 package ee.email
 
 import ee.design.gen.DesignKotlinGenerator
+import ee.lang.integ.dPath
 import ee.lang.integ.eePath
 
 fun main(args: Array<String>) {
     val generator = DesignKotlinGenerator(Email)
-    generator.generate(eePath)
+    generator.generate(dPath)
 }
