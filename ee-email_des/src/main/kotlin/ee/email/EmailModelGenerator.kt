201ba4647f4243452b42bbfe2a0d2c28964d1540
Eugen Eisler <eoeisler@gmail.com>
2017-05-09 21:32:00 +0200

    Work on Go generation

--- ee-email_des/src/main/kotlin/ee/email/EmailModelGenerator.kt
+++ ee-email_des/src/main/kotlin/ee/email/EmailModelGenerator.kt
@@ -1,10 +1,20 @@
 package ee.email
 
-import ee.design.gen.DesignKotlinGenerator
+import ee.design.gen.go.DesignGoGenerator
+import ee.design.gen.kt.DesignKotlinGenerator
 import ee.lang.integ.dPath
-import ee.lang.integ.eePath
 
 fun main(args: Array<String>) {
-    val generator = DesignKotlinGenerator(Email)
+    generateGo()
+}
+
+fun generateKotlin() {
+    var generator = DesignKotlinGenerator(Email)
     generator.generate(dPath)
 }
+
+fun generateGo() {
+    var generator = DesignGoGenerator(Email)
+    generator.generate(dPath)
+}
+
