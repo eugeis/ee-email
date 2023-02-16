f3bb245fb880724cc4557c2dea543c866a82da5b
Eugen Eisler <eoeisler@gmail.com>
2023-02-16 11:55:14 +0100

    Working state

--- ee-email/src/main/kotlin/ee/email/EmailExt.kt
+++
@@ -1,9 +0,0 @@
-package ee.email
-
-fun EmailAddress.name(): String {
-    return address.substringBefore("@")
-}
-
-fun EmailAddress.domain(): String {
-    return address.substringAfter("@")
-}
--- ee-email/src/main/kotlin/ee/email/SharedApi.kt
+++
@@ -1,3 +0,0 @@
-package ee.email
-
-
